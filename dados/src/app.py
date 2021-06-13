720b9e082437176eed55341eb7354e462f37ad96
daianeklein <daiane.klein22@gmail.com>
2021-06-12 22:50:51 -0300

    correcao caminho

--- dados/src/app.py
+++ dados/src/app.py
@@ -32,8 +32,8 @@ def grafico_comparativo(dados_2019, dados_2020, causa, estado="BRASIL"):
     #grafico_comparativo("SRAG")
 
 def main():
-    obitos_2019 = carrega_dados("/Users/daianeklein/Documents/Personal-Projects/Covid-Streamlit/analise-covid/dados/obitos-2019.csv")
-    obitos_2020 = carrega_dados("/Users/daianeklein/Documents/Personal-Projects/Covid-Streamlit/analise-covid/dados/obitos-2020.csv")
+    obitos_2019 = carrega_dados("dados/obitos-2019.csv")
+    obitos_2020 = carrega_dados("dados/obitos-2020.csv")
     tipo_doenca = obitos_2020['tipo_doenca'].unique()
     estado = np.append(obitos_2020['uf'].unique(), "BRASIL")
 
